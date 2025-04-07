62646250c9f46ac76cbff7652f644a81b54dfa34
PR9INICHEK <pr9inichek@gmail.com>
2025-04-07 20:04:47 +0300

    Update start.sh
    https://discord.com/channels/105462288051380224/976119935875026964/1358848060037529610

--- server-files/start.sh
+++ server-files/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start server
 cd server-files
-./ArmaReforgerServer -config ~/conflict.json -profile ~/profile -maxFPS 60 -logStats 60000
+./ArmaReforgerServer -config /home/armaserver/conflict.json -profile /home/armaserver/profile -maxFPS 60 -logStats 60000
