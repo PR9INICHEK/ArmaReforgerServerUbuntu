3fcb44751e4f3480ab8d2078bb36e35bfd19ed41
PR9INICHEK <pr9inichek@gmail.com>
2025-05-07 20:12:37 +0300

    Update start.sh

--- server-files/start.sh
+++ server-files/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start server
 cd server-files
-./ArmaReforgerServer -config /home/armarserver/conflict.json -profile /home/armarserver/profile -maxFPS 60 -logStats 60000
+./ArmaReforgerServer -config /home/armarserver/conflict.json -profile /home/armarserver/profile -maxFPS 60 -logStats 60000 -logTime datetime -autoreload 60 -autoshutdown
